cf2f71a07f3b105a9144915d690e1ed337d18b68
mateosbodio <141843461+mateosbodio@users.noreply.github.com>
2023-08-17 12:00:12 -0300

    ahi agregue la consigna

---
+++ tp2.java
@@ -0,0 +1,11 @@
+/* Crear un repositorio en GitHub: Un miembro del equipo debe crear un nuevo proyecto en Netbeans y desde GitHub Desktop crear el repositorio. Puedes darle un nombre descriptivo y agregar una breve descripción.
+Compartir el repositorio con los demás miembros: El miembro que creó el repositorio debe invitar a los demás miembros del equipo como colaboradores en la configuración del repositorio en GitHub.
+Instalar GitHub Desktop: Todos los miembros del equipo deben descargar e instalar GitHub Desktop en sus computadoras desde el sitio web oficial (desktop.github.com).
+Clonar el repositorio en GitHub Desktop: Cada miembro del equipo debe abrir GitHub Desktop y seleccionar la opción "Clone a repository" (Clonar un repositorio). Selecciona el repositorio que creaste en GitHub y elige una ubicación en tu computadora para clonarlo.
+Importar el proyecto en NetBeans: Cada miembro del equipo debe abrir NetBeans y seleccionar "File" (Archivo) en la barra de menú. Luego, elige "Open Project" (Abrir Proyecto) y selecciona la carpeta del repositorio clonado en tu computadora que contiene el proyecto de NetBeans.
+Realizar cambios y confirmarlos: Cada miembro del equipo puede trabajar en el proyecto en NetBeans y realizar cambios en los archivos. Cuando estén listos para confirmar los cambios, deben regresar a GitHub Desktop, seleccionar el repositorio clonado y realizar el proceso de confirmación con un resumen descriptivo y una descripción opcional.
+Sincronizar con el repositorio remoto: Después de confirmar los cambios, cada miembro debe seleccionar la opción "Push origin" (Enviar a origen) en GitHub Desktop para enviar los cambios al repositorio remoto en GitHub.
+Actualizar y fusionar cambios: Cuando otro miembro del equipo haya realizado cambios en el repositorio remoto, cada miembro debe seleccionar la opción "Pull origin" (Recibir desde origen) en GitHub Desktop para obtener los últimos cambios.
+Este ejercicio les permitirá a los miembros del equipo trabajar en el mismo proyecto de NetBeans de manera colaborativa utilizando GitHub Desktop. Recuerden comunicarse y coordinar los cambios para evitar conflictos. ¡Diviértanse trabajando juntos!
+
+ */
