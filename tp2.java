367603c3e7bca792040aae8cef545cf227e25765
SkinnyGuille <guillermo.n.rosales@gmail.com>
2023-08-17 12:44:26 -0300

    prueba de main

--- tp2.java
+++ tp2.java
@@ -13,7 +13,7 @@ public class tp2 {
 
     public static void main(String[] args) {
         System.out.println("Hola mundoo");
-        
+        System.out.println("Como estan?");
         
     }
 }
